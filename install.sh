67fdb0e0b20ed72b1d91ee1f4a4098b54d0b791b
octa <octaliz@gmail.com>
2017-09-25 13:51:22 +0200

    Test

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 mkdir -p ~/.vim/colors
-mkdir -p ~/.vim/bundle
-git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#mkdir -p ~/.vim/bundle
+#git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone --depth=1 https://github.com/octabaka/dotfiles.git ~/dotfiles
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/octa.vim ~/.vim/colors/octa.vim
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+#ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-mkdir ~/.zfunctions
-ln -s ~/dotfiles/pure.zsh ~/.zfunctions/prompt_pure_setup
-ln -s ~/dotfiles/async.zsh ~/.zfunctions/async
+#mkdir ~/.zfunctions
+#ln -s ~/dotfiles/pure.zsh ~/.zfunctions/prompt_pure_setup
+#ln -s ~/dotfiles/async.zsh ~/.zfunctions/async
 ln -s ~/dotfiles/mpv.conf ~/.config/mpv/mpv.conf
 ln -s ~/dotfiles/input.conf ~/.config/mpv/input.conf
 
 vim +PluginInstall +qall
 
-python ~/.vim/bundle/install.py --system-boost --tern-completer
+#python ~/.vim/bundle/install.py --system-boost --tern-completer
 
 
 
