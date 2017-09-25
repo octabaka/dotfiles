1abee2b71622cd8a96387a75c3b739c570d2ef2f
octabaka <octaliz@gmail.com>
2017-09-25 17:58:12 +0200

    Nvim & Oh-my-zsh

---
+++ .nvimrc
@@ -0,0 +1,45 @@
+call plug#begin('~/.local/share/nvim/plugged')
+" Make sure you use single quotes
+Plug 'cloudhead/neovim-fuzzy'
+Plug 'mattn/emmet-vim'
+Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'dracula/vim'
+Plug 'jiangmiao/auto-pairs'
+call plug#end()
+
+let g:AutoPairsCenterLine = 0
+"set termguicolors
+let g:airline_powerline_fonts = 1
+let g:airline#extensions#tabline#enabled = 0 
+let g:airline_theme='luna'
+"let g:airline#extensions#whitespace#checks = [ 'indent', 'trailing', 'long', 'mixed-indent-file' ]
+let g:airline#extensions#whitespace#enabled = 0
+colors dracula 
+
+set path=.,**
+set wildignore+=*/tmp/*,*.so,*.swp,*.zip
+set expandtab
+set tabstop=4
+set shiftwidth=4
+set clipboard+=unnamedplus
+set scrolloff=3
+set relativenumber
+set cursorline
+set gcr=a:blinkon500-blinkwait500-blinkoff500 
+set ignorecase
+set noshowmode 
+set hidden
+
+autocmd BufReadPost * if @% !~# '\.git[\/\\]COMMIT_EDITMSG$' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif 
+
+
+
+noremap <C-p> :FuzzyOpen<CR>
+nnoremap - :Ex<CR>
+
+let g:deoplete#enable_at_startup = 1
+
+
+
--- install.sh
+++ install.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
+git clone --depth=1 https://github.com/octabaka/dotfiles.git ~/dotfiles
 
-mkdir -p ~/.vim/colors
+# ZSH
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+wget https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/spaceship.zsh -O ~/.oh-my-zsh/themes/spaceship.zsh-theme
+
+# VIM
+#mkdir -p ~/.vim/colors
 #mkdir -p ~/.vim/bundle
 #git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone --depth=1 https://github.com/octabaka/dotfiles.git ~/dotfiles
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/octa.vim ~/.vim/colors/octa.vim
-#ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-#mkdir ~/.zfunctions
-#ln -s ~/dotfiles/pure.zsh ~/.zfunctions/prompt_pure_setup
-#ln -s ~/dotfiles/async.zsh ~/.zfunctions/async
+#ln -s ~/dotfiles/.vimrc ~/.vimrc
+#ln -s ~/dotfiles/octa.vim ~/.vim/colors/octa.vim
+#vim +PluginInstall +qall
+
+# NVIM
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/nvim/colors
+ln -s ~/dotfiles/.nvimrc ~/.config/nvim/init.vim
+ln -s ~/dotfiles/octa.vim ~/.config/nvim/colors/octa.vim
+nvim +PlugInstall +qall
+
+# MPV
+mkdir -p ~/.config/mpv
 ln -s ~/dotfiles/mpv.conf ~/.config/mpv/mpv.conf
 ln -s ~/dotfiles/input.conf ~/.config/mpv/input.conf
 
-vim +PluginInstall +qall
-
-#python ~/.vim/bundle/install.py --system-boost --tern-completer
 
 
+
+# TMUX
+#ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 exit 0
